b31f95d914b41cba1f59a4ac20f842752f7466d7
Andrew Trigg <andrewt51@hotmail.com>
2015-11-19 13:32:31 -0800

    Schema's crypto and jwt methods complete

--- templateFront/models/user.js
+++ templateFront/models/user.js
@@ -1,9 +1,35 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var jwt = require('jsonwebtoken');
+var crypto = require('crypto');
 
 var User = new Schema({
   name: String,
-  age: Number
+  age: Number,
+  hash:String,
+  salt:String
 });
+
+User.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2sync(password,this.salt,1000,64).toString('hex');
+};
+
+User.methods.validPassword = function(password){
+  var hash = crypto.pbkdf2sync(password,this.salt,1000,64).toString('hex');
+  return hash === this.hash;
+};
+
+User.methods.generateJWT = function(){
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    _id: this._id,
+    name: this.name,
+    exp: parseInt(exp.getTime()/1000)
+  }, process.env.SECRET);
+};
 
 module.exports = mongoose.model('User',User);
--- templateFront/routes/users.js
+++ templateFront/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -10,10 +11,13 @@ router.get('/', function(req, res, next) {
 router.post('/addUser', function(req,res){
   User.create({
     name: req.body.name,
-    age: req.body.age
+    age: req.body.age,
+ 
   }, function(err,user){
     err ? res.send(err) : res.send(user);
   })
 })
+
+
 
 module.exports = router;
