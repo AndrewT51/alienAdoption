c7a80100233014c276aafeca8695dbd2487a9437
Andrew Trigg <andrewt51@hotmail.com>
2015-12-09 20:47:23 +0000

    Added functionality to show logged in user

--- templateFront/public/js/main.js
+++ templateFront/public/js/main.js
@@ -20,18 +20,40 @@ var myApp = angular.module('myApp',['ui.router'])
     controller: 'loginCtrl'
   })
 
+   .state('users.test',{
+    url:'/test',
+    templateUrl: '../templates/test.html',
+    controller: 'testCtrl'
+  })
+
 })
 
 .controller('navCtrl',function($scope,$state){
   console.log($state.current.name)
   $scope.stateName = $state.current
+  $scope.$on('log', function(_,user){
+    $scope.currentUser = JSON.parse(atob(user.data.split('.')[1])).name;
+    console.log('Look: ', $scope.currentUser)
+  })
+})
+
+.controller('testCtrl', function(){
+  // $scope.currentUser
 })
 
 .controller('loginCtrl',function($scope, $http, userService){
   var theUser = $scope.user;
   $scope.submit = function(user){
-    $scope.user = '';
+    // $scope.user = '';
     userService.login(user,'users','login')
+    .then(function successCallback(response) {
+      localStorage.token = JSON.stringify(response);
+    $scope.$emit('log', response);
+      console.log(JSON.parse(localStorage.token))
+    }, function errorCallback(error) {
+      console.log(error)
+    });
+
   }
 
 })
--- templateFront/public/js/services.js
+++ templateFront/public/js/services.js
@@ -2,11 +2,11 @@ var myApp = angular.module('myApp')
 .service('userService', function ($http, $state) {
   this.login = function (data,url1,url2) {
     return $http.post(url1 + '/' + url2, data)
-    .then(function successCallback(response) {
-      localStorage.token = JSON.stringify(response);
-      console.log(JSON.parse(localStorage.token))
-    }, function errorCallback(error) {
-      console.log(error)
-    });
+    // .then(function successCallback(response) {
+    //   localStorage.token = JSON.stringify(response);
+    //   console.log(JSON.parse(localStorage.token))
+    // }, function errorCallback(error) {
+    //   console.log(error)
+    // });
   }
 })
